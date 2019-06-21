0c072fbaaa589e422a2369d48bbe2ee6fe75f713
Unknown <jacktgarland@gmail.com>
2019-06-21 18:11:14 +0100

    Tiny update
    Just a really small update that has no real effect, I am going away for the weekend and so need to move it to my laptop to carry on work.

--- PHP/Match.php
+++ PHP/Match.php
@@ -1,7 +1,7 @@
+<?php
 // This will be used to match two player's. 
 // Find a player with the avaible flag in the database, set to false and add user to match.
 // Then display awaiting insult page, set new session varible that states they are in a match so we don't have to ping the database each time.
-<?php
 session_start();
 function matchCheck(){
     if ($_SESSION["matched"] == null){
